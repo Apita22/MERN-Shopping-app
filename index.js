4b5f848936aed6085f7724a8989946562016761d
Arpita <arpitadhirendra89602gmail.com>
2022-06-27 12:08:05 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
-const express = require('express')
-const app = express()
-const products =require('./assets/products.json')
-
-
-app.get('/', function (req, res) {
-  res.send('hello')
-   })
-    let cat=[];
-    app.get('/getproducts/', (req, res)=> { 
-       let i = products.filter((data)=> {
-
-    if(data.category==="jewelery"){
-      cat.push(data);
-    }
-
-  
-     })
-        res.send(cat)
-    
- });
-app.listen(3000);
+require('./config/config');
+require('./models/DB');
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.listen(process.env.PORT);
