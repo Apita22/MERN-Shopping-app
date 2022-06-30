a850b1f8ad3ef713a972caad3c44ee22a842eaaa
Arpita <arpitadhirendra89602gmail.com>
2022-06-30 13:50:33 +0530

    save

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 require('./config/config');
-// require('./models/DB');
+require('./models/DB');
+ const express = require('express');
 const routes=require('./routes/index.router')
-const product=require('./routes/product.route')
-const express = require('express');
 const app = express();
 app.use(express.json());
 app.use('/api',routes);
-  app.use('/product',product)
 app.listen(process.env.PORT);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // const express = require("express");
 // const products = require("./assets/products.json");
 // const app = express();
--- routes/product.route.js
+++
@@ -1,5 +0,0 @@
-const express =require('express');
-const product =require('../controller/product');
-const router=express.Router();
-router.get('/:id',product.getOneProduct);
-module.exports=router;
